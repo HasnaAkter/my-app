5cac306ce8066183e5260de934d5f2e040ef7bdd
HasnaAkter <hasnaakter.cse@gmail.com>
2023-04-25 20:55:26 +0600

    nextreact

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+const name = 'Hasna';
+const number =22;
+const singer={name:'SM', job:'SQA', titale:'gadha'}
+
+const singerStyle ={
+  color: 'red',
+  backgroundColor: 'white'
+
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         
         <img src={logo} className="App-logo" alt="logo" />
         <h1>jsx</h1>
@@ -14,22 +23,57 @@ function App() {
           <p></p>
         </div>
 
-       <div className="container"></div>
+       <div className="container"><h3>hello react it's my 1st react</h3></div>
+        <div className="music">
+          <p>Name: {name} </p>
+          <p style={
+            {color: 'yellow',backgroundColor: 'red'}
+          }>Number: { number}</p>
+          <p style={singerStyle}>Singer: {singer.name} {singer.job} {singer.titale}</p>
+        </div>
         
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </header> */}
+    
+    <Person name='Hena' nayka="cheka"></Person>
+    <Person name='happy' nayka="moushumi"></Person>
+    <Person name='Maraz' nayka="moni"></Person>
+    <Person name='hero' nayka="khan"></Person>
+    <h1 > New Component.YaY</h1>
+    <h1 id="newcm"> View Sorce</h1>
+    <Friend name="jui" number="1"></Friend>
+    <Friend name="liya" number="2"></Friend>
+    <Friend name = "hena" number="3"></Friend>
+    <Friend name = "maraz" number="4"></Friend>
+    
+
     </div>
   );
 }
+
+function Person(props){
+  console.log(props);
+  return (
+  <div className='person'>
+    <h1> {props.name} </h1>
+  <p>Nayka: {props.nayka} </p>
+  </div>
+  
+  )
+
+}
+
+
+function Friend(props){
+  console.log(props);
+  return (
+  <div className='friend'>
+    <h1> {props.name} </h1>
+  <p> {props.number} </p>
+  </div>
+  
+  )
+
+}
+
 
 export default App;
